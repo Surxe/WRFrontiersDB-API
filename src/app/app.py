0dc2dd92089fe2102b3b39589952254111fc102d
Surxe <119145352+Surxe@users.noreply.github.com>
2025-06-12 23:04:26 -0500

    use port from env

--- src/app/app.py
+++ src/app/app.py
@@ -18,4 +18,7 @@ def items():
     return jsonify([dict(item) for item in items])
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
